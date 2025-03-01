5ee2b34638335cb11fccbdb87271a07073661846
= <sriyaroy@yahoo.com>
2025-03-01 19:03:09 +0000

    saving docker file

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ from fastapi.responses import FileResponse
 from PIL import Image, ImageOps
 import shutil
 import os
+import logging
+
+logger = logging.getLogger('SupaRes')
+logger.setLevel(logging.INFO)
 
 app = FastAPI()
 
@@ -14,8 +18,9 @@ def dummy_preprocess(image_path: str) -> str:
         grey_img.save(ouptut_path)
     return ouptut_path
 
-@app.post("/upload")
+@app.post("/")
 async def upload(file: UploadFile = File(...)):
+    logger.info(f"Received file: {file.filename}")
     # save the uploaded file locally
     temp_file_path = os.path.join('uploads', file.filename)
     with open(temp_file_path, "wb") as buffer:
@@ -23,5 +28,6 @@ async def upload(file: UploadFile = File(...)):
 
     # preprocess the image
     output_path = dummy_preprocess(temp_file_path)
+    logger.info(f"Processed file: {output_path}")
 
     return FileResponse(path=output_path, filename='processed.png')
