6c8eb61e840e3fd5ad01b5c09969cad09cad7cc7
= <sriyaroy@yahoo.com>
2025-03-09 15:20:17 +0000

    fixing logs to appear in stdout

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 from fastapi import FastAPI, File, UploadFile
 from fastapi.responses import FileResponse
 import shutil
-import os
+import sys
 import logging
 import preprocess
 
 logger = logging.getLogger('SupaRes')
-logger.setLevel(logging.INFO)
+logger.setLevel(logging.INFO) # Without StreamHandler this doesn't send the logs anywhere
+
+# Attach a StreamHandler to send logs to STDOUT if none exists yet
+if not logger.handlers:
+    console_handler = logging.StreamHandler(sys.stdout)
+    formatter = logging.Formatter("%(levelname)s - %(message)s")
+    console_handler.setFormatter(formatter)
+    logger.addHandler(console_handler)
 
 app = FastAPI()
 
@@ -21,7 +28,7 @@ async def upload(file: UploadFile = File(...)):
     # preprocess the image
     output_img = preprocess.dimCheck(temp_file_path)
     output_path = preprocess.dummy_greyscale(output_img, temp_file_path)
-    logger.info(f"Processed file: {output_path}")
+    logger.info(f'Sending processed file to user: {output_path}')
 
     return FileResponse(path=output_path, filename='processed.png')
 
